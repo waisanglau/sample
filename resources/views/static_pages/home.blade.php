02f856ad4e5bf1e7d05e560cf31e1fa37d68739b
liuweisheng <1056820563@qq.com>
2018-10-02 13:09:16 +0800

    Finish static pages

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+  <h1>主页123</h1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@
 Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
